7f4424a10ebe7da6b0762a3f49057ad24037f02d
Shruti <shruti.rohila@globallogic.com>
2023-04-26 10:00:12 +0530

    commit

---
+++ questions.js
@@ -0,0 +1,98 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the full form of DBMS?",
+    answer: "Database Management System",
+    options: [
+      "Data of Binary Management System",
+      "Database Management System",
+      "Database Management Service",
+      "Data Backup Management System",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which type of data can be stored in the database?",
+    answer: "All of the above",
+    options: [
+      "Image oriented data",
+      "Text, files containing data",
+      " Data in the form of audio or video",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "Which of the following is not a type of database?",
+    answer: "Decentralized",
+    options: ["Hierarchical", "Network", "Distributed", "Decentralized"],
+  },
+  {
+    numb: 6,
+    question: "Which of the following is not an example of DBMS?",
+    answer: "Google",
+    options: ["MySQL", "Microsoft Acess", "IBM DB2", "Google"],
+  },
+  {
+    numb: 7,
+    question: "Which of the following is not  a feature of DBMS?",
+    answer: "Single-user Access only",
+    options: [
+      "Minimum Duplication and Redundancy of Data",
+      "High Level of Security",
+      "Single-user Access only",
+      "Support ACID Property",
+    ],
+  },
+  {
+    numb: 8,
+    question: "Which of the following is a component of the DBMS?",
+    answer: "All of the above",
+    options: ["Data", "Data Languages", " Data Manager", "All of the above"],
+  },
+  {
+    numb: 9,
+    question: " What does an RDBMS consist of?",
+    answer: "Collection of Tables",
+    options: [
+      "Collection of Records",
+      "Collection of Keys",
+      "Collection of Fields",
+      "Collection of Tables",
+    ],
+  },
+  {
+    numb: 10,
+    question:
+      "The ability to query data, as well as insert, delete, and alter tuples, is offered by ",
+    answer: " DML (Data Manipulation Langauge)",
+    options: [
+      "TCL (Transaction Control Language)",
+      "DCL (Data Control Language)",
+      "DDL (Data Definition Langauge)",
+      "DML (Data Manipulation Langauge)",
+    ],
+  },
+];
---
+++ script.js
@@ -0,0 +1,143 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+
+start_btn.onclick = () => {
+  info_box.classList.add("activeInfo");
+};
+
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+};
+
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.add("activeQuiz");
+  showQuetions(0);
+};
+
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = () => {
+  quiz_box.classList.add("activeQuiz");
+  result_box.classList.remove("activeResult");
+  que_count = 0;
+  que_numb = 1;
+  userScore = 0;
+  widthValue = 0;
+  showQuetions(que_count);
+  queCounter(que_numb);
+
+  next_btn.classList.remove("show");
+};
+
+quit_quiz.onclick = () => {
+  window.location.reload();
+};
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+next_btn.onclick = () => {
+  if (que_count < questions.length - 1) {
+    que_count++;
+    que_numb++;
+    showQuetions(que_count);
+    queCounter(que_numb);
+    next_btn.classList.remove("show");
+  } else {
+    showResult();
+  }
+};
+
+function showQuetions(index) {
+  const que_text = document.querySelector(".que_text");
+
+  let que_tag =
+    "<span>" +
+    questions[index].numb +
+    ". " +
+    questions[index].question +
+    "</span>";
+  let option_tag =
+    '<div class="option"><span>' +
+    questions[index].options[0] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[1] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[2] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    questions[index].options[3] +
+    "</span></div>";
+  que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
+
+  const option = option_list.querySelectorAll(".option");
+
+  for (i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer) {
+  let userAns = answer.textContent;
+  let correcAns = questions[que_count].answer;
+  const allOptions = option_list.children.length;
+
+  if (userAns == correcAns) {
+    userScore += 1;
+    answer.classList.add("correct");
+    answer.insertAdjacentHTML("beforeend", tickIconTag);
+    console.log("Correct Answer");
+    console.log("Your correct answers = " + userScore);
+  } else {
+    answer.classList.add("incorrect");
+    answer.insertAdjacentHTML("beforeend", crossIconTag);
+    console.log("Wrong Answer");
+
+    for (i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correcAns) {
+        option_list.children[i].setAttribute("class", "option correct");
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag);
+        console.log("Auto selected correct answer.");
+      }
+    }
+  }
+  for (i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+  next_btn.classList.add("show");
+}
+
+function showResult() {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.remove("activeQuiz");
+  result_box.classList.add("activeResult");
+  const scoreText = result_box.querySelector(".score_text");
+
+  let scoreTag =
+    "<span> You got <p>" +
+    userScore +
+    "</p> out of <p>" +
+    questions.length +
+    "</p></span>";
+  scoreText.innerHTML = scoreTag;
+}
